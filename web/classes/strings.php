ac5f4f254fde69d79c2c86915421374138b38e4d
polosat <dmitry@stepanov.info>
2015-03-10 21:00:12 +0300

    Tallinn: Login/Profile is implemented

--- web/classes/strings.php
+++ web/classes/strings.php
@@ -26,34 +26,3 @@ abstract class Strings {
     return constant($this->className . '::' . strtoupper($name));
   }
 }
-
-//abstract class Strings {
-//  static private $instances = array();
-//  protected $className;
-//
-//  protected function __construct($className) {
-//    $this->className = $className;
-//  }
-//
-//  static public function GetInstance($language) {
-//    $defaultClass = get_called_class();
-//    $localizedClass = empty($language) ? $defaultClass : ($defaultClass . '_' . $language);
-//
-//    if (isset(self::$instances[$localizedClass])) {
-//      return self::$instances[$localizedClass];
-//    }
-//    else if (class_exists($localizedClass)) {
-//      return (self::$instances[$localizedClass] = new $localizedClass($localizedClass));
-//    }
-//    else if (isset(self::$instances[$defaultClass])) {
-//      return self::$instances[$defaultClass];
-//    }
-//    else {
-//      return (self::$instances[$defaultClass] = new $defaultClass($defaultClass));
-//    }
-//  }
-//
-//  public function GetConstant($name) {
-//    return constant($this->className . '::' . strtoupper($name));
-//  }
-//}
